2ff31a396ebd9338c39187bcb47ebf480c858c18
Matthew Rice <matt.rice@unity3d.com>
2026-04-21 19:58:01 -0700

    added right click menu option

--- background.js
+++ background.js
@@ -1,3 +1,21 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: 'copy-jira-link',
+    title: 'Copy Jira issue link',
+    contexts: ['page'],
+    documentUrlPatterns: [
+      '*://*.atlassian.net/browse/*',
+      '*://*/browse/*',
+    ],
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === 'copy-jira-link' && tab) {
+    chrome.tabs.sendMessage(tab.id, { action: 'copyJiraLink' });
+  }
+});
+
 chrome.commands.onCommand.addListener((command) => {
   if (command === 'copy-jira-link') {
     chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
